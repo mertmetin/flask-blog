6658c249287ac02b2dda5fb2fcc75372db956a3d
mertmetin@outlook.com <mertmetin@outlook.com>
2014-07-08 17:53:16 -0400

    blog and user seperated

--- config.py
+++ config.py
@@ -1,6 +1,7 @@
 class Configuration(object):
     MONGODB_SETTINGS = {
-        'DB': 'mertmetin'
+        'DB': 'mertmetin',
+        'PORT': 27018
     }
     DEBUG = True
     SECRET_KEY = 'e46de46c63ff4fa2830019e34cd4fccf'
